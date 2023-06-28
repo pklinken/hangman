019898aa930313461d726f27118b1de8d46badc5
Paul Klinkenberg <pkl@commentor.dk>
2023-06-28 19:24:52 +0200

    Refactor to Nicer C

--- hangman.c
+++ hangman.c
@@ -2,10 +2,11 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <time.h>
 
 #define MAX_WORDS 1000
-#define TRUE 1
-#define FALSE 0
+#define MAX_ERROR_COUNT 8
 
 const char hangman[] =
 	" ____   \n"
@@ -35,14 +36,14 @@ void init_str(char *str, char init_char, int len)
 	}
 }
 
-int has_char(char *str, char c)
+bool has_char(char *str, char c)
 {
 	for (int i = 0; i < strlen(str); i++)
 	{
 		if (str[i] == c)
-			return TRUE;
+			return true;
 	}
-	return FALSE;
+	return false;
 }
 
 int prompt_user()
@@ -64,31 +65,39 @@ int prompt_user()
 	return in;
 }
 
-void load_wordlist(char ***result, int *wordcount)
+struct WordList
 {
-	char **lines = xmalloc(sizeof(char *) * MAX_WORDS);
-	FILE *wordlist = fopen("./wordlist", "r");
+	char **words;
+	size_t len;
+};
 
-	int line_count = 0;
+struct WordList load_wordlist()
+{
+	struct WordList result;
+	result.words = xmalloc(sizeof(char *) * MAX_WORDS);
+	result.len = 0;
+
+	FILE *f_wordlist = fopen("./wordlist", "r");
+
 	do
 	{
 		char *line = 0;
 		size_t line_length = 0;
 
-		if (getline(&line, &line_length, wordlist) < 0)
+		if (getline(&line, &line_length, f_wordlist) < 0)
 			break;
 
-		lines[line_count++] = line;
-	} while (line_count < MAX_WORDS);
+		result.words[result.len++] = line;
+	} while (result.len < MAX_WORDS);
 
-	printf("Read %i lines from file\n", line_count);
+	printf("Read %lu lines from file\n", result.len);
 
-	fclose(wordlist);
+	fclose(f_wordlist);
 
-	*result = lines;
-	*wordcount = line_count;
+	return result;
 }
 
+// Cells in the hangman gfx to blank
 int levels[8][6] = {
 	{41, -1},
 	{39, -1},
@@ -100,10 +109,10 @@ int levels[8][6] = {
 	{10, 19, 28, 37, 46, -1},
 };
 
-int *get_blank_indices(int error_count)
+void get_blank_indices(int error_count, int **indices, int *count)
 {
-	int *indices = xmalloc(sizeof(int) * 8 * 6);
-	int i = 0;
+	*indices = xmalloc(sizeof(int) * 8 * 6);
+	*count = 0;
 	int level = 7 - error_count;
 
 	while (level >= 0)
@@ -111,17 +120,13 @@ int *get_blank_indices(int error_count)
 		int level_index = 0;
 		while (levels[level][level_index] != -1)
 		{
-			indices[i] = levels[level][level_index];
-			i++;
+			(*indices)[*count] = levels[level][level_index];
+			(*count)++;
 			level_index++;
 		}
 
 		level--;
 	}
-
-	indices[i] = -1;
-
-	return indices;
 }
 
 void print_hangman(int error_count)
@@ -129,13 +134,13 @@ void print_hangman(int error_count)
 	char *hangman_copy = xmalloc(strlen(hangman) + 1);
 	strcpy(hangman_copy, hangman);
 
-	int *indices_to_blank = get_blank_indices(error_count);
+	int *indices_to_blank, count;
+	get_blank_indices(error_count, &indices_to_blank, &count);
 
-	int i = 0;
-	while (indices_to_blank[i] != -1)
+	while (count > 0)
 	{
-		hangman_copy[indices_to_blank[i]] = ' ';
-		i++;
+		hangman_copy[indices_to_blank[count - 1]] = ' ';
+		count--;
 	}
 
 	printf("%s", hangman_copy);
@@ -146,63 +151,57 @@ void print_hangman(int error_count)
 
 void print_word(char *word, char *guesses)
 {
-	char *output = xmalloc(sizeof(char) * strlen(word) - 1);
+	char *output = xmalloc(sizeof(char) * strlen(word));
 	for (int i = 0; i < strlen(word) - 1; i++)
-	{
-		output[i] = '_';
-		for (int j = 0; j < strlen(guesses); j++)
-		{
-			if (word[i] == guesses[j])
-				output[i] = word[i];
-		}
-	}
+		output[i] = has_char(guesses, word[i]) ? word[i] : '_';
+
 	output[strlen(word) - 1] = '\0';
 
 	printf("Guess the word: %s\n", output);
 	free(output);
 }
 
-int check_for_win(char *word, char *guesses)
+bool check_for_win(char *word, char *guesses)
 {
 	for (int i = 0; i < strlen(word) - 1; i++)
 	{
 		if (!has_char(guesses, word[i]))
-			return FALSE;
+			return false;
 	}
 
-	return TRUE;
+	return true;
 }
 
 int main()
 {
-	int error_count = 0;
+	// get wordlist
+	struct WordList wordlist = load_wordlist();
 
-	char **word_list;
-	int word_count;
-
-	load_wordlist(&word_list, &word_count);
-
-	// printf("Words available:\n");
-	// for (int i = 0; i < word_count; i++)
-	// {
-	// 	printf("%i: %s", i + 1, word_list[i]);
-	// }
-	// printf("\n");
-
+	// set random seed and pick random word
 	srand(time(0));
-	char *word = word_list[rand() % word_count];
+	int random_index = rand() % wordlist.len;
+	// printf("%i", strlen(wordlist.words[random_index]));
+	char *word_to_guess = xmalloc(sizeof(char *) * strlen(wordlist.words[random_index]) - 1);
+	// Copy to new buffer and strip the \n
+	// The string from getline would be "abc\n" so strlen() returns 4 but the buffer length is 5
+	strncpy(word_to_guess, wordlist.words[random_index], strlen(wordlist.words[random_index]) - 1);
+	word_to_guess[strlen(wordlist.words[random_index]) - 1] = '\0';
 
-	// printf("%s", word);
+	// Free the memory we allocated so far
+	for (int i = 0; i < wordlist.len; i++)
+		free(wordlist.words[i]);
+	free(wordlist.words);
 
-	char *guesses = xmalloc(sizeof(char) * 26);
-	init_str(guesses, '\0', 26);
+	// Up to 26 guesses
+	char *guesses = xmalloc(sizeof(char) * 27);
+	init_str(guesses, '\0', 27);
 
 	int user_input;
-
-	while (1)
+	int error_count = 0;
+	while (true)
 	{
 		print_hangman(error_count);
-		print_word(word, guesses);
+		print_word(word_to_guess, guesses);
 		if (strlen(guesses) > 0)
 			printf("Previous guesses: %s\n", guesses);
 
@@ -219,31 +218,29 @@ int main()
 
 			if (has_char(guesses, (char)user_input))
 			{
-				printf("You already guessed this letter!");
+				printf("You already guessed this letter!\n");
 				continue;
 			}
 			else
 				guesses[strlen(guesses)] = (char)user_input;
 
-			if (check_for_win(word, guesses))
+			if (check_for_win(word_to_guess, guesses))
 			{
-				printf("---¤¤ %s ¤¤--- Well done!!\n", word);
+				printf("---¤¤ %s ¤¤---\nWell done!!\n", word_to_guess);
 				return 0;
 			}
-			if (!has_char(word, (char)user_input))
-			{
+
+			if (!has_char(word_to_guess, (char)user_input))
 				error_count++;
-			}
-			if (error_count == 8)
+
+			if (error_count == MAX_ERROR_COUNT)
 			{
-				print_hangman(error_count);
-				printf("Game over! The answer was %s\n", word);
+				print_hangman(MAX_ERROR_COUNT);
+				printf("Game over! The answer was %s\n", word_to_guess);
 				return 0;
 			}
 		}
 	}
 
-	// Free words ??
-	free(word_list);
 	return 0;
 }
